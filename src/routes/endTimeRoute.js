d88caef58448d0ff9646a8513a26629c89e87946
SShreeC <123401235+SShreeC@users.noreply.github.com>
2025-06-16 08:54:56 +0530

    Update endTimeRoute.js

--- src/routes/endTimeRoute.js
+++ src/routes/endTimeRoute.js
@@ -1,16 +1,4 @@
-// // routes/endTimeRoutes.js
-// const express = require('express');
-// const router = express.Router();
-// const { addEndTimes, getEndTimes ,deleteEndTime,updateEndTime} = require('../controllers/endTimeController');
-// const { authenticateToken } = require('../utils/jwtUtils');
 
-// // Route for adding/updating end times
-// router.post('/addEndTime', authenticateToken, addEndTimes);
-// router.put('/updateEndTime', authenticateToken, updateEndTime); 
-// router.delete('/deleteEndTime', authenticateToken, deleteEndTime);
-// router.get('/getEndTime', authenticateToken, getEndTimes);
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const { addEndTimes, getEndTimes, updateEndTime, deleteEndTime } = require('../controllers/endTimeController');
